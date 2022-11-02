b9eb5e19bc1f9a053cad58ee994c2fdd450381a3
Stebbi <stefodd@yahoo.com>
2022-11-02 17:38:22 -0500

    implemented scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from 'react-icons/fa'
 import {HiOutlineMail} from "react-icons/hi";
 import {BsFillPersonLinesFill} from "react-icons/bs"
 import Logo from '../assets/logo.png';
+import {Link} from 'react-scroll';
 
 const Navbar =() => {
     const [nav, setNav] = useState(false);
@@ -17,11 +18,31 @@ const Navbar =() => {
             {/* menu */}
             <div className="hidden md:flex">
                 <ul className="hidden md:flex">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Skills</li>
-                    <li>Work</li>
-                    <li>Contact</li>
+                    <li>
+                    <Link to="home" smooth={true} duration={500}>
+                        Home
+                    </Link>
+                    </li>
+                    <li>
+                    <Link to="about" smooth={true} duration={500}>
+                        About
+                    </Link>
+                    </li>
+                    <li>
+                    <Link to="skills" smooth={true} duration={500}>
+                        Skills
+                    </Link>
+                    </li>
+                    <li>
+                    <Link to="work" smooth={true} duration={500}>
+                        Work
+                    </Link>
+                    </li>
+                    <li>
+                    <Link to="contact" smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                    </li>
                 </ul>
             </div>
             {/* Hamburger */}
@@ -30,33 +51,48 @@ const Navbar =() => {
             </div>
             {/* Mobile Menu */}
             <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
-                <li className="py-6 text-4xl">Home</li>
-                <li className="py-6 text-4xl">About</li>
-                <li className="py-6 text-4xl">Skills</li>
-                <li className="py-6 text-4xl">Work</li>
-                <li className="py-6 text-4xl">Contact</li> 
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                        About
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+                        Skills
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+                        Work
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li> 
             </ul>
 
             {/* Social Icons */}
             <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
                 <ul>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
-                        <a className="flex justify-between items-center w-full text-grey-300" href="/">
+                        <a className="flex justify-between items-center w-full text-grey-300" href="https://www.linkedin.com/in/stefan-oddsson-96a861140/">
                             LinkedIn <FaLinkedin size={30} />
                         </a>
                     </li>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
-                        <a className="flex justify-between items-center w-full text-grey-300" href="/">
+                        <a className="flex justify-between items-center w-full text-grey-300" href="https://github.com/StebbiO">
                             Github <FaGithub size={30} />
                         </a>
                     </li>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
-                        <a className="flex justify-between items-center w-full text-grey-300" href="/">
-                            Email <HiOutlineMail size={30} />
-                        </a>
-                    </li>
-                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
-                        <a className="flex justify-between items-center w-full text-grey-300" href="/">
+                        <a className="flex justify-between items-center w-full text-grey-300" href="https://docs.google.com/document/d/1vYPB-3KDRFiD9K7-BSjgkXKmtMm316TGaFupbWOofaA/edit">
                             Resume <BsFillPersonLinesFill size={30} />
                         </a>
                     </li>
