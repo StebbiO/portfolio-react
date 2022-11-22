7459091911cb4a17689b371558eb1ec1e36406fd
Stebbi <stefodd@yahoo.com>
2022-11-21 18:06:06 -0600

    edited bios

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <p className='text-pink-600'>Hello, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Stefan Oddsson</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>Full-stack web developer with a background working in the legal field. Completed coding bootcamp through the University of Texas and earned a certificate in full-stack web development. Willing team-player and fast learner using attention to detail to build strong foundations and clean code for web applications.</p>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>Full-stack web developer with a background working in the legal field. Willing team-player and fast learner using attention to detail to build strong foundations and clean code for web applications.</p>
             <div>
                 <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work
                     <span className='group-hover:rotate-90 duration-300'>
