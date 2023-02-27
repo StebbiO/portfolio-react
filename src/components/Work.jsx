fc29d77023b7d2cf976838024032f03b3e14179c
Stebbi <stefodd@yahoo.com>
2023-02-27 17:45:28 -0600

    add pizza app

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,6 +3,7 @@ import Poke from '../assets/pokehunt.png'
 import Book from '../assets/books.jpg'
 import Video from '../assets/video-game.jpg'
 import Blog from '../assets/tech-blog.jpg'
+import Pizza from '../assets/pizza.jpg'
 
 function Work() {
   return (
@@ -92,6 +93,26 @@ function Work() {
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
                             <a href='https://github.com/StebbiO/just-tech-news'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div
+                 style={{backgroundImage: `url(${Pizza})`}}
+                 className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+                >
+                    
+                    {/*Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            MongoDB Application
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='https://fierce-sierra-58331.herokuapp.com/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='https://github.com/StebbiO/pizza-hunt2'>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
